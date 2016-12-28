1e5a670cd53c904e4e2863b076e29a14a7dfa042
Mariordk <vazquezonrubia@hotmail.com>
2016-12-28 03:04:52 +0100

    Ya se borran los vuelos consultados.

--- app/src/main/java/com/example/mario/airpports/MainActivity.java
+++ app/src/main/java/com/example/mario/airpports/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mario.airpports;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -66,5 +70,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.delete:
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle(getString(R.string.delete_flights));
+
+                alertDialogBuilder
+                        .setMessage(getString(R.string.confirm_delete))
+                        .setCancelable(false)
+                        .setPositiveButton(getString(R.string.yes),new DialogInterface.OnClickListener() {
+                            // Lo que sucede si se pulsa yes
+                            public void onClick(DialogInterface dialog, int id) {
+                                int uri = getContentResolver().delete(FlightContract.CONTENT_URI,null,null);
+                                Toast toast1 = Toast.makeText(getApplicationContext(), getString(R.string.number_deleted_flights) + uri, Toast.LENGTH_LONG);
+                                toast1.show();
+                            }
+
+                        })
+                        .setNegativeButton(getString(R.string.No),new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // Si se pulsa no hace nada
+                                dialog.cancel();
+                            }
+                        });
+
+                //Se crea el alertDialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                //Se muestra
+                alertDialog.show();
+
+                return true;
+            default:
+                return false;
+        }
     }
 }
