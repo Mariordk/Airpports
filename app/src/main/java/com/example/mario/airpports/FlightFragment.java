1e7691fe389bbf1feaa09ca3940a182e0ffe90c1
Mariordk <vazquezonrubia@hotmail.com>
2017-01-04 17:18:48 +0100

    Añadido que al hacer click en uno de los vuelos consultados vuela a mostrarse esa misma ruta. En ocasiones falla pero no se porqué. Siguen faltando comentarios.

--- app/src/main/java/com/example/mario/airpports/FlightFragment.java
+++ app/src/main/java/com/example/mario/airpports/FlightFragment.java
@@ -1,6 +1,8 @@
 package com.example.mario.airpports;
 
 
+import android.app.LauncherActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -10,6 +12,7 @@ import android.support.v4.content.Loader;
 import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
@@ -21,6 +24,8 @@ public class FlightFragment extends ListFragment implements LoaderManager.Loader
     private static final String TAG = FlightFragment.class.getSimpleName();
 
     private SimpleCursorAdapter mAdapter;
+    TextView numero_vuelo;
+    TextView fecha_vuelo;
 
     private static final String[] FROM = {FlightContract.Column.NUMERO_VUELO, FlightContract.Column.FECHA_VUELO, FlightContract.Column.FECHA_CONSULTA,FlightContract.Column.VELOCIDAD_MAXIMA,FlightContract.Column.ALTURA_MAXIMA};
     private static final int[] TO = {R.id.list_item_numero_vuelo, R.id.list_item_fecha_vuelo, R.id.list_item_fecha_consulta,R.id.list_item_velocidad_maxima,R.id.list_item_altitud_maxima};
@@ -29,11 +34,13 @@ public class FlightFragment extends ListFragment implements LoaderManager.Loader
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
+
         super.onActivityCreated(savedInstanceState);
         mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item, null, FROM, TO, 0);
         mAdapter.setViewBinder(new TimelineViewBinder());
         setListAdapter(mAdapter);
         getLoaderManager().initLoader(LOADER_ID, null, this);
+        setEmptyText(getString(R.string.no_seen_routes));
     }
 
     @Override
@@ -67,6 +74,22 @@ public class FlightFragment extends ListFragment implements LoaderManager.Loader
             ((TextView) view).setText(relativeTime);
             return true;
         }
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int pos, long id) {
+
+        numero_vuelo = (TextView) this.getListView().getChildAt(pos).findViewById(R.id.list_item_numero_vuelo);
+        fecha_vuelo = (TextView) this.getListView().getChildAt(pos).findViewById(R.id.list_item_fecha_vuelo);
+        Intent intent = new Intent(this.getActivity(), MapsActivity.class);
+        //Estos son los parametros que se pasan al activity del maps para poder usarlos alli
+        //Vuelo seleccionado y fecha seleccionada
+        intent.putExtra("numero_vuelo", String.valueOf(numero_vuelo.getText()));
+        intent.putExtra("fecha_vuelo", String.valueOf(fecha_vuelo.getText()));
+        //Iniciamos la nueva actividad
+        startActivity(intent);
+        super.onListItemClick(l, v, pos, id);
+
     }
 }
 
